173b648afecf3964bdbbc36edc8fe78e1a9fdc3f
Haruto Adaniya <e215707@ie.u-ryukyu.ac.jp>
2022-01-15 00:02:25 +0900

    Poker2_CardSet_Correction

--- app/src/main/java/jp/ac/uryukyu/ie/e215707/PlayingCards.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215707/PlayingCards.java
@@ -2,25 +2,57 @@ package jp.ac.uryukyu.ie.e215707;
 import java.util.ArrayList;
 import java.util.Collections;
 public class PlayingCards{
-    private String[] design = {"♠️", "❤️", "♣️", "♦️"};
-    private String[] number = {"1", "2", "3", "4", "5", "6", "7", "8","9", "10", "11", "12", "13"};
-    ArrayList<String> hand_cards = new ArrayList<>();
-    ArrayList<String> cards = new ArrayList<>();
-    void playgame(){
+    private String[] design = {"♠️", "❤️", "♣️", "♦️"}; 
+    private String[] number = {"1", "2", "3", "4", "5", "6", "7", "8","9", "10", "11", "12", "13"}; 
+    ArrayList<ArrayList<String>> hand_cards_1P = new ArrayList<ArrayList<String>>(); 
+    ArrayList<ArrayList<String>> hand_cards_2P = new ArrayList<ArrayList<String>>(); 
+    ArrayList<ArrayList<String>> cards = new ArrayList<ArrayList<String>>(); 
+    
+    ArrayList<ArrayList<String>> getCards(){
+        return cards;
+    }
+
+    void playgame(){ 
         for(int d = 0; d < 4; d++){
             for(int n = 0; n < 13; n++){
-                cards.add(design[d] + number[n]);
+                ArrayList<String> card = new ArrayList<>();
+                card.add(design[d]);
+                card.add(number[n]);
+                cards.add(card);
                 }
             }
-            Collections.shuffle(cards);
+            Collections.shuffle(cards); 
+
         for(int I = 0; I < 5; I++){
             int i = 0;
-            hand_cards.add(cards.get(i));
-            System.out.println(I+1 + ":" + cards.get(i));
+            ArrayList<String> hand_card_1P = new ArrayList<>();
+            for(int c=0; c<2; c++){
+                hand_card_1P.add(cards.get(i).get(c));
+                }
+            hand_cards_1P.add(hand_card_1P);
             cards.remove(i);
+        }
+        
+        for(int I = 0; I < 5; I++){
+            int i = 0;
+            ArrayList<String> hand_card_2P = new ArrayList<>();
+            for(int c=0; c<2; c++){
+            hand_card_2P.add(cards.get(i).get(c));
             }
+            hand_cards_2P.add(hand_card_2P);
+            cards.remove(i);
+        }
+        for(int i=0; i<5; i++){
+            System.out.println(i +1 + ":" +hand_cards_1P.get(i));
+            }
+        
+        for(int i=0; i<5; i++){
+            System.out.println(i +1 + ":" +hand_cards_2P.get(i));
+            }
+            
         }
     }
+    
 
 
 
